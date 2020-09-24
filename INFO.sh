21c232ac24dbed864db45301049f241eef49c094
shooter-dev <31919820+shooter-dev@users.noreply.github.com>
2020-09-24 15:32:51 +0200

    init

---
+++ INFO.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2000-2016 Synology Inc. All rights reserved.
+
+source /pkgscripts/include/pkg_util.sh
+
+UISTRING_PATH="/source/syno-library/package/ui/texts"
+# Avec la version 6.1, la description et le nom sont obligatoires et ne peuvent plus être construit à partir des fichiers de traduction.
+displayname=Mangatheque
+description=Gestion-Mangatheque
+
+package="Mangatheque"
+version="0.0.0005"
+maintainer="Synology Community"
+arch="$(pkg_get_platform)"
+
+dsmuidir="ui"
+dsmappname="SYNOCOMMUNITY.sl.AppMangatheque"
+
+thirdparty="yes"
+
+install_dep_services="nginx pgsql"
+install_dep_packages="PythonModule"
+start_dep_services="nginx pgsql"
+
+[ "$(caller)" != "0 NULL" ] && return 0
+pkg_dump_info
