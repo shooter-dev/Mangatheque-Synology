18730b4c89e0e7eed2d60137d0d38fe17aeab22e
shooter-dev <31919820+shooter-dev@users.noreply.github.com>
2020-09-24 19:03:13 +0200

    rect

--- INFO.sh
+++ INFO.sh
@@ -3,10 +3,10 @@
 
 source /pkgscripts/include/pkg_util.sh
 
-UISTRING_PATH="/source/syno-designer/package/ui/texts"
+UISTRING_PATH="/source/Mangatheque-Synology/package/ui/texts"
 # Avec la version 6.1, la description et le nom sont obligatoires et ne peuvent plus être construit à partir des fichiers de traduction.
 displayname=syno-designer
-description=syno-designer
+description="Gestion d'une Mangatheque pour les utilisatu-eur du serveur Synology"
 
 package="syno-designer"
 version="0.0.0001"
