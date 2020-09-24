011bdfa7d21c9011922596eee705ef26e1616f22
shooter-dev <31919820+shooter-dev@users.noreply.github.com>
2020-09-24 20:20:16 +0200

    up database

--- package/scripts/sql/create.sql
+++ package/scripts/sql/create.sql
@@ -20,4 +20,59 @@ CREATE TABLE IF NOT EXISTS t_component (
     "container"     BOOLEAN DEFAULT FALSE
 );
 
+CREATE TABLE IF NOT EXISTS Users (
+    "id"            BIGINT PRIMARY KEY,
+    "name"     	    varchar(100) NOT NULL,
+    "firstname"     varchar(100) NOT NULL,
+    "sex"           char(1) NOT NULL,
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Editeurs (
+    "id"            BIGINT PRIMARY KEY,
+    "name"          varchar(512) NOT NULL,
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Genres (
+    "id"            BIGINT PRIMARY KEY,
+    "name"          varchar(512) NOT NULL,
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Types (
+    "id"            BIGINT PRIMARY KEY,
+    "name"          varchar(512) NOT NULL,
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Personnes (
+    "id"            BIGINT PRIMARY KEY,
+    "name"     	    varchar(100) NOT NULL,
+    "firstname"     varchar(100) NOT NULL,
+    "sex"           char(1) NOT NULL,
+    "naissance"     datetime,
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Series (
+    "id"            BIGINT PRIMARY KEY,
+    "name"     	    varchar(512) NOT NULL,
+    "volumes"       int (11),
+    "cover"         varchar(512) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Tomes (
+    "code"          varchar(13) PRIMARY KEY,
+    "name"     	    varchar(512) NOT NULL,
+    "volume"        float(11) NOT NULL,
+    "pages"         int(11) NOT NULL,
+    "date_VF"       datetime,
+    "date_VO"       datetime,
+    "cover"         varchar(512) NOT NULL
+);
+
+
+
+
 COMMIT ;
