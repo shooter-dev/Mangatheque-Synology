b697d058f51eb8d56eea01f58bda1646ae3715c0
shooter-dev <31919820+shooter-dev@users.noreply.github.com>
2020-09-25 22:59:48 +0200

    up

--- package/scripts/sql/create.sql
+++ package/scripts/sql/create.sql
@@ -34,6 +34,12 @@ DROP  TABLE IF NOT EXISTS Series ();
 
 DROP  TABLE IF NOT EXISTS Tomes ();
 
+DROP  TABLE IF NOT EXISTS SerieGenre ();
+
+DROP  TABLE IF NOT EXISTS Travails ();
+
+DROP  TABLE IF NOT EXISTS Roles ();
+
 CREATE TABLE IF NOT EXISTS Users (
     "id"            BIGINT PRIMARY KEY,
     "name"     	    varchar(255) NOT NULL,
@@ -73,7 +79,8 @@ CREATE TABLE IF NOT EXISTS Series (
     "id"            BIGINT PRIMARY KEY,
     "name"     	    varchar(255) NOT NULL,
     "volumes"       float (11) NOT NULL,
-    "cover"         varchar(255) NOT NULL
+    "cover"         varchar(255) NOT NULL,
+    "GenreId"       int(11) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Tomes (
@@ -83,11 +90,40 @@ CREATE TABLE IF NOT EXISTS Tomes (
     "pages"         int(11) NOT NULL,
     "date_VF"       datetime,
     "date_VO"       datetime,
-    "resumer"       varchar(3000)
-    "cover"         varchar(255) NOT NULL
+    "resumer"       varchar(3000),
+    "cover"         varchar(255) NOT NULL,
+    "EditeurId"     int(11) NOT NULL,
+    "SerieId"       int(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SeriesGenres (
+    "GenreId"       int(11) NOT NULL,
+    "SerieId"       int(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Travails (
+    "PersonneId"    int(11) NOT NULL,
+    "TomeCode"      varchar(13) NOT NULL
 );
 
 
+CREATE TABLE IF NOT EXISTS Roles (
+    "id"            BIGINT PRIMARY KEY,
+    "role"     	    varchar(255) NOT NULL,
+    "cover"         varchar(255) NOT NULL,
+);
 
+ALTER TABLE SerieGenres
+  ADD CONSTRAINT `SerieGenres_ibfk_1` FOREIGN KEY (`GenreId`) REFERENCES `Genres` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `SerieGenres_ibfk_2` FOREIGN KEY (`SerieId`) REFERENCES `Series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE Tomes
+  ADD CONSTRAINT `Tomes_ibfk_1` FOREIGN KEY (`EditeurId`) REFERENCES `Editeurs` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `Tomes_ibfk_2` FOREIGN KEY (`SerieId`) REFERENCES `Series` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE Travails
+  ADD CONSTRAINT `Travails_ibfk_1` FOREIGN KEY (`PersonneId`) REFERENCES `Personnes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Travails_ibfk_2` FOREIGN KEY (`TomeCode`) REFERENCES `Tomes` (`code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Travails_ibfk_3` FOREIGN KEY (`RoleId`) REFERENCES `Roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 COMMIT ;
