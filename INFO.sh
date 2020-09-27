f93d01a1631618976998273eda4f1b3c46b0373c
shooter-dev <31919820+shooter-dev@users.noreply.github.com>
2020-09-27 20:31:25 +0200

    up

--- INFO.sh
+++ INFO.sh
@@ -5,37 +5,22 @@ source /pkgscripts/include/pkg_util.sh
 
 UISTRING_PATH="/source/Mangatheque-Synology/package/ui/texts"
 # Avec la version 6.1, la description et le nom sont obligatoires et ne peuvent plus être construit à partir des fichiers de traduction.
-displayname=mangatheque
+displayname=syno-designer
+description=syno-designer
 
-package="mangatheque"
-
+package="syno-designer"
 version="0.0.0001"
-
-maintainer="ShooterDev"
-
-maintainer_url="http://shooterdev.fr"
-
-distributor="ShooterDev"
-
-support_url="http://shooterdev.fr"
-
+maintainer="Synology Community"
 arch="$(pkg_get_platform)"
 
 dsmuidir="ui"
+dsmappname="SYNOCOMMUNITY.sl.AppInstance"
 
-dsmappname="SYNO.SDS.MANGATHEQUE"
+thirdparty="yes"
 
 install_dep_services="nginx pgsql"
-
 install_dep_packages="PythonModule"
+start_dep_services="nginx pgsql"
 
-start_dep_services="nginx pgsql" 
-
-thirdparty = "yes" 
-
-support_conf_folder = "yes"
-
-description="Gestion d'une Mangatheque pour les Utilisateurs du Super Serveur Synology"
 [ "$(caller)" != "0 NULL" ] && return 0
-
 pkg_dump_info
